46bd027b2d217bf66c9a4678631a4aac8481c073
nufikharizkic <nufikharizki@gmail.com>
2023-12-03 10:55:03 +0700

    Update FIle

---
+++ cypress/e2e/web/magento/Login/Login_Invalid_Password.cy.js
@@ -0,0 +1,10 @@
+describe('Verify Login', () => {
+  it('Failed Login with Wrong Password', () => 
+  {
+    cy.visit('https://magento.softwaretestingboard.com/customer/account/login/referer/aHR0cHM6Ly9tYWdlbnRvLnNvZnR3YXJldGVzdGluZ2JvYXJkLmNvbS8%2C/')
+    cy.get('#email').type('nufikharizki@gmail.com')
+    cy.get('#pass').type('0000abc')
+    cy.get('#send2').click()
+    cy.get('.message-error').should('contain','The account sign-in was incorrect or your account is disabled temporarily. Please wait and try again later.')
+  })
+});
---
+++ cypress/e2e/web/magento/Login/Verify_Forgot_Password.cy.js
@@ -0,0 +1,11 @@
+describe('Verify Forgot Password Functionality', () => {
+    it('Successfully Changed', () => 
+    {
+      cy.visit('https://magento.softwaretestingboard.com/customer/account/login/referer/aHR0cHM6Ly9tYWdlbnRvLnNvZnR3YXJldGVzdGluZ2JvYXJkLmNvbS8%2C/')
+      cy.get('#email').type('nufikharizki@gmail.com')
+      cy.get('.secondary > .action > span').click()
+      cy.get('#email_address').type('nufikharizki@gmail.com')
+      cy.get('#form-validate > .actions-toolbar > div.primary > .action > span').click()
+      cy.get('.message-success').should('If there is an account associated with nufikharizki@gmail.com you will receive an email with a link to reset your password.')
+    })
+  });
